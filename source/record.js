ca1eff81cd4faf4c41aa578d59b9b81ea350e22f
chad-tung <chad.tung@gmail.com>
2017-12-01 14:07:49 +0000

    made constructor for record and passed test.

---
+++ source/record.js
@@ -0,0 +1,5 @@
+var Record = function(artist) {
+  this.artist = artist;
+}
+
+module.exports = Record;
--- specs/record_spec.js
+++ specs/record_spec.js
@@ -2,6 +2,10 @@ var assert = require('assert');
 var Record = require('../source/record.js')
 
 describe("Record test", function() {
+  var record;
+  beforeEach(function() {
+    record = new Record("Chad");
+  });
   it("should have an artist", function() {
     assert.strictEqual(record.artist, "Chad");
   });
