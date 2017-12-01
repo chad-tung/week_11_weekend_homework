b518b72360e8884ae2a0255fb6d657b6d20fceb1
chad-tung <chad.tung@gmail.com>
2017-12-01 14:23:45 +0000

    wrote test for store name

--- specs/store_spec.js
+++ specs/store_spec.js
@@ -2,5 +2,7 @@ var assert = require('assert');
 var Store = require('../source/store.js');
 
 describe("Store test", function() {
-
+  it('should have a name', function() {
+    assert.strictEqual(store.name, "Big Al's Record Store")
+  });
 });
