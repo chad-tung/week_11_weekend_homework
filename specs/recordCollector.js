0771d5b20c58d5b13fd369b21c9b78a315e08359
chad-tung <chad.tung@gmail.com>
2017-12-03 20:42:31 +0000

    set up test and constructor

---
+++ specs/recordCollector.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+var RecordCollector = require('../source/recordCollector.js')
+
+describe("Record collector test", function() {
+  beforeEach(function() {
+    collector = new RecordCollector('Pierce')
+  })
+})
