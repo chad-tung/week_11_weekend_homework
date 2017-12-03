62e04e4896a136e8452a95c9d9786d972b72c806
chad-tung <chad.tung@gmail.com>
2017-12-03 21:01:14 +0000

    wrote sell function and passed test

--- specs/recordCollector_spec.js
+++ specs/recordCollector_spec.js
@@ -4,16 +4,27 @@ var Record = require('../source/record.js');
 
 describe("Record collector test", function() {
   var record;
+  var record2;
   var collector;
   beforeEach(function() {
     collector = new RecordCollector("Pierce");
 
     record = new Record("Chad", "Chopin Op.25 Etudes", "Classical", 15);
+
+    record2 = new Record("Jazzy McJazz", "Jazz hits", "Jazz", 20);
   });
 
   it('should be able to buy a record', function() {
     assert.deepStrictEqual(collector.inventory, []);
     collector.buyRecord(record);
+    assert.deepStrictEqual(collector.inventory, [record]);
+  });
+
+  it('should be able to sell a record', function() {
+    collector.buyRecord(record);
+    collector.buyRecord(record2);
+    assert.deepStrictEqual(collector.inventory, [record, record2]);
+    collector.sellRecord(record2);
     assert.deepStrictEqual(collector.inventory, [record]);
   });
 
